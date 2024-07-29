c6f36700c197d8440a1bdaef94510fdd9a0e0421
giulldev <giugiucarvalho0305@gmail.com>
2024-07-29 20:30:00 -0300

    Criação de Classes Iniciais

---
+++ src/main/java/com/itb/mif3an/pizzaria/model/Produto.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.pizzaria.model;
+
+public class Produto {
+
+    public Long id;
+    public String nome;
+    public String tipo;
+    public String descricao;
+    public double precoCompra;
+    public double precoVenda;
+    public int quantidadeEstoque;
+    public boolean codStatus;
+}
